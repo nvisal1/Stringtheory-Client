abc5e66f7b94e94392b9df6b27cc121034a094df
Nick Visalli <nvisal1@students.towson.edu>
2019-08-03 15:43:23 -0400

    scaffold component structure and install react-router-dom

---
+++ src/home/Home.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home: React.FC = () => {
+    return (
+        <div>
+            Hello React App from scratch
+        </div>
+    );
+}
+
+export default Home;
