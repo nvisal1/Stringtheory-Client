be28be831055884cecef82a79f9b23e3228b97a3
Nick Visalli <nvisal1@students.towson.edu>
2019-08-11 23:15:56 -0400

    format home page header

---
+++ src/curriculum/components/course-dashboard/course-dashboard.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './course-dashboard.css';
+
+const CourseDashboard: React.FC = () => {
+    return (
+        <div className='course-dashboard'>
+            {/* <Header /> */}
+            {/* <Courses /> */}
+        </div>
+    );
+}
+
+export default CourseDashboard;
